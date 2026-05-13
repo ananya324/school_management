ad5e990371035c9f639839169fadae21b062dfdb
Anya <sanodiaananya@gmail.com>
2026-05-13 22:56:10 +0530

    fix

--- backend/controllers/schoolController.js
+++ backend/controllers/schoolController.js
@@ -3,32 +3,29 @@ const calculateDistance = require("../utils/distance");
 
 exports.addSchool = (req, res) => {
     const { name, address, latitude, longitude } = req.body;
-    if (
-        !name ||-
-        !address ||
-        latitude == null ||
-        longitude == null
-    ) {
+
+    if (!name || !address || latitude == null || longitude == null) {
         return res.status(400).json({
             message: "All fields are required",
         });
     }
 
-    const query = "INSERT INTO schools (name, address, latitude, longitude) VALUES (?, ?, ?, ?)";
-    db.query(
-        query,
-        [name, address, latitude, longitude],
-        (err, result) => {
-            if (err) {
-                return res.status(500).json({
-                    message: "Database error",
-                });
-            }
-            res.status(201).json({
-                message: "School added successfully",
+    const query =
+        "INSERT INTO schools (name, address, latitude, longitude) VALUES (?, ?, ?, ?)";
+
+    db.query(query, [name, address, latitude, longitude], (err, result) => {
+        if (err) {
+            console.log("DB ERROR:", err);
+            return res.status(500).json({
+                message: "Database error",
+                error: err.sqlMessage || err.message,
             });
         }
-    );
+
+        res.status(201).json({
+            message: "School added successfully",
+        });
+    });
 };
 
 exports.listSchools = (req, res) => {
@@ -39,12 +36,16 @@ exports.listSchools = (req, res) => {
             message: "Latitude and Longitude required",
         });
     }
-    db.query("SELECT *FROM schools", (err, results) => {
+
+    db.query("SELECT * FROM schools", (err, results) => {
         if (err) {
+            console.log("DB ERROR:", err);
             return res.status(500).json({
                 message: "Database error",
+                error: err.sqlMessage || err.message,
             });
         }
+
         const schools = results.map((school) => {
             const distance = calculateDistance(
                 parseFloat(latitude),
@@ -52,13 +53,15 @@ exports.listSchools = (req, res) => {
                 school.latitude,
                 school.longitude
             );
+
             return {
                 ...school,
                 distance: distance.toFixed(2),
             };
         });
+
         schools.sort((a, b) => a.distance - b.distance);
 
         res.status(200).json(schools);
-    })
-}
+    });
+};
